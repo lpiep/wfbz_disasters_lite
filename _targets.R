792fc80b14add6eaf051c79e852a14008a205dc9
lpiep <loganap@uw.edu>
2024-07-11 17:47:09 -0700

    create paths before save

--- _targets.R
+++ _targets.R
@@ -23,6 +23,7 @@ list(
     name = event_fema_raw, # see https://www.fema.gov/api/open/v1/OpenFemaDataSetFields?$filter=openFemaDataSet%20eq%20%27FemaWebDisasterSummaries%27%20and%20datasetVersion%20eq%201 for metadata
     {
 	    dst <- 'data/raw/nonstatic/event/fema.csv'
+	    dir_create(path_dir(dst))
     	download.file(
     		'https://www.fema.gov/api/open/v2/DisasterDeclarationsSummaries.csv',
     		dst
@@ -36,6 +37,7 @@ list(
   	name = event_ics_raw_current_year,
   	{
   		dst <- 'data/raw/nonstatic/event/ics_raw.json'
+  		dir_create(path_dir(dst))
   		httr::GET(
   			url = 'https://famdwh-dev.nwcg.gov/sit209/cognos_report_queries/sit209_data_report',
   			authenticate('famdwhapiusr', 'Welcome1234!', type = "basic"), # non-secret authentication
@@ -51,7 +53,7 @@ list(
   	name = spatial_mtbs_raw,
   	unzip_url(
   		'https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/MTBS_Fire/data/composite_data/burned_area_extent_shapefile/mtbs_perimeter_data.zip',
-  		'data/raw/nonstatic/spatial/mtbs'  	
+  		dir_create('data/raw/nonstatic/spatial/mtbs')  	
   	),	
   	format = 'file',
   	cue = tar_cue(mode = getOption('wilfire_disasters_lite.cue_downloads'))
@@ -60,7 +62,7 @@ list(
   	name = spatial_fired_raw,
   	unzip_url(
   		'https://scholar.colorado.edu/downloads/h702q749s',
-  		'data/raw/nonstatic/spatial/fired'
+  		dir_create('data/raw/nonstatic/spatial/fired')
   	),	
   	format = 'file',
   	cue = tar_cue(mode = getOption('wilfire_disasters_lite.cue_downloads'))
@@ -69,7 +71,7 @@ list(
   	name = spatial_nifc_raw,
   	unzip_url(
   		'https://opendata.arcgis.com/api/v3/datasets/e02b85c0ea784ce7bd8add7ae3d293d0_0/downloads/data?format=shp&spatialRefId=4326&where=1%3D1',
-  		'data/raw/nonstatic/spatial/nifc'
+  		dir_create('data/raw/nonstatic/spatial/nifc')
   	),	
   	format = 'file',
   	cue = tar_cue(mode = getOption('wilfire_disasters_lite.cue_downloads'))
@@ -79,7 +81,7 @@ list(
   	name = spatial_tiger_counties_2020_raw,
   	unzip_url(
   		'https://www2.census.gov/geo/tiger/TIGER2020/COUNTY/tl_2020_us_county.zip',
-  		'data/reference/county_2020'
+  		dir_create('data/reference/county_2020')
   	),	
   	format = 'file',  	
   	cue = tar_cue(mode = "never") # expected to be static
@@ -88,7 +90,7 @@ list(
   	name = spatial_tiger_counties_2010_raw,
   	unzip_url(
   		'https://www2.census.gov/geo/tiger/TIGER2010/COUNTY/2010/tl_2010_us_county10.zip',
-  		'data/reference/county_2010'
+  		dir_create('data/reference/county_2010')
   	),	
   	format = 'file',
   	cue = tar_cue(mode = "never") # expected to be static
@@ -98,7 +100,7 @@ list(
   	name = spatial_tiger_counties_2000_raw,
   	unzip_url(
   		'https://www2.census.gov/geo/tiger/TIGER2010/COUNTY/2000/tl_2010_us_county00.zip',
-  		'data/reference/county_2000'
+  		dir_create('data/reference/county_2000')
   	),	
   	format = 'file',
   	cue = tar_cue(mode = "never") # expected to be static
@@ -108,7 +110,7 @@ list(
   	name = spatial_tiger_counties_1990_raw,
   	unzip_url(
   		'https://www2.census.gov/geo/tiger/PREVGENZ/co/co90shp/co99_d90_shp.zip',
-  		'data/reference/county_1990'
+  		dir_create('data/reference/county_1990')
   	),	
   	format = 'file',
   	cue = tar_cue(mode = "never") # expected to be static
