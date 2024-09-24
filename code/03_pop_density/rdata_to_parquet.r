506fad3a6b3cd6de7b3747035dfd6916efa0ae21
lpiep <loganap@uw.edu>
2024-09-24 13:08:16 -0700

    hide rdata_to_parquet from targets pipeline

--- code/03_pop_density/rdata_to_parquet.r
+++ code/03_pop_density/rdata_to_parquet.r
@@ -6,7 +6,7 @@
 # Author: Lauren Wilner
 # Date: 2024-08-24
 
-
+if(FALSE){
 #-------------------------
 # setup
 library(arrow)
@@ -24,6 +24,8 @@ st_write_parquet(all_disaster_perimeters_buffers_conus_dist_select_vars,
 st_write_parquet(all_disaster_perimeters_buffers_alaska_dist_select_vars,
     paste0(path, "all_disaster_perimeters_buffers_hawaii_dist_select_vars.parquet"))
 st_write_parquet(all_disaster_perimeters_buffers_hawaii_dist_select_vars,
-    paste0(path"all_disaster_perimeters_buffers_hawaii_dist_select_vars.parquet"))
+    paste0(path, "all_disaster_perimeters_buffers_hawaii_dist_select_vars.parquet"))
 st_write_parquet(all_disaster_with_ics_poo_no_acreage_cont_us_select_vars,
     paste0(path,"all_disaster_with_ics_poo_no_acreage_cont_us_select_vars.parquet"))
+
+}
