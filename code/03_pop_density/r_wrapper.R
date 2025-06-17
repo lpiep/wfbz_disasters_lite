789401a033a754acfe85c87f3d0c3a8c9acc2e76
Logan Piepmeier <loganap@uw.edu>
2025-06-17 11:00:54 -0700

    wui working

--- code/03_pop_density/r_wrapper.R
+++ code/03_pop_density/r_wrapper.R
@@ -1,7 +1,8 @@
-apply_pop_density <- function(spatial, pop_density_py_script, ...){
-	tempfile <- 'data/02_processed/spatial.geojson'
+apply_pop_density <- function(data, pop_density_py_script, ...){
+	tempfile <- 'data/02_processed/wui.geojson'
 	if(file_exists(tempfile)) file_delete(tempfile)
-	write_sf(spatial, 'data/02_processed/spatial.geojson')
+	write_sf(data, tempfile)
 	system(paste(conda, "run -n wf python", pop_density_py_script, '-o data'))
+	unlink(tempfile)
 	return('data/02_processed/fire_pop_density_criteria.csv')
 }
